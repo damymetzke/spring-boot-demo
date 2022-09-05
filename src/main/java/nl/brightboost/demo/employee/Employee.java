40a8db7e84827ecb18aa205070d892907f615043
Damy Metzke <damy@brightboost.nl>
2022-09-05 12:19:13 +0200

    Add `Employee` entity

---
+++ src/main/java/nl/brightboost/demo/employee/Employee.java
@@ -0,0 +1,17 @@
+package nl.brightboost.demo.employee;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Employee {
+    @Id
+    @SequenceGenerator(name = "employee_sequence", sequenceName = "employee_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_sequence")
+
+    private Long id;
+
+}
